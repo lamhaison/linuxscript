084b769eed4a215ca4f1c5c435c99801a6a34c5e
lamhaison <lamhaison@gmail.com>
2016-06-14 16:56:48 +0700

    Update create_mysql_accout.sh
    script create account to access mysql

--- create_mysql_accout.sh
+++ create_mysql_accout.sh
@@ -6,3 +6,4 @@ mysql -u root -e 'create database $DATABASE_NAME'
 mysql -u root -e "CREATE USER '$USERNAME'@'$ACCESS_IP' IDENTIFIED BY '$PASSWORD'"
 mysql -u root -e "GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$USERNAME'@'$ACCESS_IP' WITH GRANT OPTION"
 mysql -u root -e "SHOW GRANTS FOR '$USERNAME'@'$ACCESS_IP'"
+
