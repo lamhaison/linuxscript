b87fffe53a108b6666ba0fbb18794b1d5e765d23
lamhaison <lamhaison@gmail.com>
2016-06-14 16:55:55 +0700

    Create create_mysql_accout.sh
    script create dabase mysql

---
+++ create_mysql_accout.sh
@@ -0,0 +1,8 @@
+USERNAME=lamhaison
+PASSWORD=lamhaison
+DATABASE_NAME=lamhaison
+ACCESS_IP=localhost
+mysql -u root -e 'create database $DATABASE_NAME'
+mysql -u root -e "CREATE USER '$USERNAME'@'$ACCESS_IP' IDENTIFIED BY '$PASSWORD'"
+mysql -u root -e "GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$USERNAME'@'$ACCESS_IP' WITH GRANT OPTION"
+mysql -u root -e "SHOW GRANTS FOR '$USERNAME'@'$ACCESS_IP'"
