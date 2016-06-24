4e243244e359a78c598309f80c29092efb63f43c
lamhaison <lamhaison@gmail.com>
2016-06-24 13:46:52 +0700

    Update rsyslog.sh
    add check exist folder

--- rsyslog.sh
+++ rsyslog.sh
@@ -4,12 +4,16 @@ LOG_NAME=haproxy.log
 LOG_FILTER=haproxy
 RSYS_LOG_CONFIG=/etc/rsyslog.d/11-haproxy.conf
 
+if [ ! -d "$LOG_DIR" ]; then
+    mkdir -p "$LOG_DIR"
+fi
+
 # Change folder log permission
 chown syslog.adm $LOG_DIR
 echo ":programname, contains, "$LOG_FILTER" $LOG_DIR/$LOG_NAME" > $RSYS_LOG_CONFIG
 service rsyslog restart
 
-cat &gt; /etc/logrotate.d/haproxy &lt;&lt; EOF
+# Add logorate /etc/logrotate.d/haproxy
 /data/haproxy/haproxy.log {
     su syslog syslog
     create 640 syslog syslog
